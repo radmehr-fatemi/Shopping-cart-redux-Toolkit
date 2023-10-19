530b150f0c2a73d1b3f8d2370ebed0e1507acdeb
Alireza Fatemi <fa1381138100@gmail.com>
2023-10-19 17:56:09 +0330

    edited

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 //Component
 import Navbar from '../header/Navbar';
-import Footer from '../footer/footer';
+import Footer from '../footer/Footer';
 
 const Layout = ({ children }) => {
     return (
